11b1938da73584e102b38d65a5b0f9f453046388
Cetins <nailsolutionsuk@gmail.com>
2022-11-03 22:01:00 +0000

    sidebar comleted

--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -12,6 +12,7 @@ const initialState = {
 export const ContextProvider = ({ children }) => {
     const [activeMenu, setActiveMenu] = useState(true);
     const [isClicked, setIsClicked] = useState(initialState);
+    const [screenSize, setScreenSize] = useState(undefined)
 
     const handleClick = (clicked) => {
         setIsClicked({ ...initialState, [clicked]: true });
@@ -19,11 +20,10 @@ export const ContextProvider = ({ children }) => {
     
     return (
         <StateContext.Provider value={{
-            activeMenu,
-            setActiveMenu,
-            isClicked,
-            setIsClicked,
-            handleClick
+            activeMenu, setActiveMenu,
+            isClicked, setIsClicked,
+            handleClick,
+            screenSize, setScreenSize
         }}>
             { children }
         </StateContext.Provider>
