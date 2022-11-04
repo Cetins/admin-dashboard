0450556b4c65a7001764d74887860868ff38361c
Cetins <nailsolutionsuk@gmail.com>
2022-11-04 16:21:17 +0000

    current color imported

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import { useStateContext } from './contexts/ContextProvider';
 import './App.css';
 
 const App = () => {
-  const { activeMenu, themeSettings, setThemeSettings } = useStateContext();
+  const { activeMenu, themeSettings, setThemeSettings, currentColor } = useStateContext();
 
   return (
     <div>
@@ -22,7 +22,7 @@ const App = () => {
             <button
                 type="button"
                 onClick={() => setThemeSettings(true)}
-                style={{ background: 'blue', borderRadius: '50%' }}
+                style={{ background: currentColor, borderRadius: '50%' }}
                 className="text-3xl text-white p-3 hover:drop-shadow-xl hover:bg-light-gray"
               >
                 <FiSettings />
