3908d857cf580c2e6ab0a2ed4447250e25ca091a
Cetins <nailsolutionsuk@gmail.com>
2022-11-04 14:56:51 +0000

    theme settings side menu connected to button

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import { useStateContext } from './contexts/ContextProvider';
 import './App.css';
 
 const App = () => {
-  const { activeMenu } = useStateContext();
+  const { activeMenu, themeSettings, setThemeSettings } = useStateContext();
 
   return (
     <div>
@@ -21,6 +21,7 @@ const App = () => {
             <TooltipComponent content="Settings" position="Top">
               <button type='button' 
               className='text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white'
+              onClick={() => setThemeSettings(true)}
               style={{ background: 'blue', borderRadius: '50%' }}>
                 <FiSettings />
               </button>
@@ -44,7 +45,7 @@ const App = () => {
             </div>
           
           <div>
-            <ThemeSettings />
+            {themeSettings && <ThemeSettings />}
             <Routes>
               {/* Dashboard */}
               <Route path='/' element={<Ecommerce />}/>
