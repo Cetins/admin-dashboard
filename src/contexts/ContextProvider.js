1c5e63f892737aedb7e8d93f2e755ddbaab85e54
Cetins <nailsolutionsuk@gmail.com>
2022-11-04 16:22:07 +0000

    color buttons bug fixed

--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -19,12 +19,14 @@ export const ContextProvider = ({ children }) => {
 
     const setMode = (e) => {
         setCurrentMode(e.target.value);
-        localStorage.setItem('themeMode, e.target.value');
+        localStorage.setItem('themeMode', e.target.value);
+        setThemeSettings(false);
     }
 
-    const setColor = (e) => {
-        setCurrentColor(e.target.value);
-        localStorage.setItem('colorMode, e.target.value');
+    const setColor = (color) => {
+        setCurrentColor(color);
+        localStorage.setItem('colorMode', color);
+        setThemeSettings(false);
     }
 
     const handleClick = (clicked) => {
@@ -37,9 +39,9 @@ export const ContextProvider = ({ children }) => {
             isClicked, setIsClicked,
             handleClick,
             screenSize, setScreenSize,
-            currentColor, setCurrentColor,
-            currentMode, setCurrentMode,
-            themeSettings, setThemeSettings
+            currentColor, currentMode, 
+            themeSettings, setThemeSettings,
+            setMode, setColor
         }}>
             { children }
         </StateContext.Provider>
